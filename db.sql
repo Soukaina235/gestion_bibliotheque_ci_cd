ba3105126be352e044f165dfa67b73569cd90e14
Soukaina Jabri <soukaina.jabri333@gmail.com>
2024-12-17 19:01:29 +0100

    updating to maven

--- db.sql
+++ db.sql
@@ -1,8 +1,22 @@
-create table student
+create table students
 (
-    id   int auto_increment,
-    name varchar(64) null,
-    constraint id
+    id    int auto_increment,
+    name  varchar(64) null,
+    email varchar(64) null,
+    constraint students_pk
         primary key (id)
 );
 
+create table borrows
+(
+    student_id  int  not null,
+    book_id     int  not null,
+    borrow_date date null,
+    return_date date null,
+    constraint borrows_pk
+        primary key (student_id, book_id),
+    constraint borrows___fk_book
+        foreign key (book_id) references books (id),
+    constraint borrows___fk_student
+        foreign key (student_id) references students (id)
+);
--- src/main/java/com/library/util/DbConnection.java
+++ src/main/java/com/library/util/DbConnection.java
@@ -5,23 +5,12 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DbConnection {
-////    private static final String URL = "jdbc:mysql://localhost:3307/library_db";
-//    private static final String LOCAL_URL = "jdbc:mysql://localhost:3307/library_db";
-//    private static final String DOCKER_URL = "jdbc:mysql://host.docker.internal:3307/library_db";
-//
-//    private static String dbUrl = System.getProperty("DB_HOST", "localhost").equals("host.docker.internal") ? DOCKER_URL : LOCAL_URL;
-//    private static final String USER = "root";
-//    private static final String PASSWORD = "llave";
-
-    private static final String URL = System.getenv("DB_URL") != null ? System.getenv("DB_URL") : "jdbc:mysql://localhost:3307/library_db";
-    private static final String USER = System.getenv("DB_USER") != null ? System.getenv("DB_USER") : "root";
-    private static final String PASSWORD = System.getenv("DB_PASSWORD") != null ? System.getenv("DB_PASSWORD") : "llave";
+    private static final String URL = "jdbc:mysql://mysql-337f9109-soukaina-e199.a.aivencloud.com:19094/library_db";
+    private static final String USER = "bibliotheque-jenkins";
+    private static final String PASSWORD = "AVNS_polljiBexINw-ILiAUI";
 
 
     public static Connection getConnection() throws SQLException {
-        System.out.println("url " + URL);
-        System.out.println("user " + USER);
-        System.out.println("password " + PASSWORD);
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             return DriverManager.getConnection(URL, USER, PASSWORD);
@@ -29,8 +18,4 @@ public class DbConnection {
             throw new SQLException("Database connection error", e);
         }
     }
-
-//    public static void main(String[] args) throws SQLException {
-//        getConnection();
-//    }
 }
