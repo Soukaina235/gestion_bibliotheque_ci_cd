02a6008ca772df83e9c7a2d20369a15ac7a933d8
Soukaina Jabri <soukaina.jabri333@gmail.com>
2024-12-17 18:18:43 +0100

    fix db error: attempt 2

--- src/main/java/com/library/util/DbConnection.java
+++ src/main/java/com/library/util/DbConnection.java
@@ -5,18 +5,23 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DbConnection {
-//    private static final String URL = "jdbc:mysql://localhost:3307/library_db";
-    private static final String LOCAL_URL = "jdbc:mysql://localhost:3307/library_db";
-    private static final String DOCKER_URL = "jdbc:mysql://host.docker.internal:3307/library_db";
+////    private static final String URL = "jdbc:mysql://localhost:3307/library_db";
+//    private static final String LOCAL_URL = "jdbc:mysql://localhost:3307/library_db";
+//    private static final String DOCKER_URL = "jdbc:mysql://host.docker.internal:3307/library_db";
+//
+//    private static String dbUrl = System.getProperty("DB_HOST", "localhost").equals("host.docker.internal") ? DOCKER_URL : LOCAL_URL;
+//    private static final String USER = "root";
+//    private static final String PASSWORD = "llave";
 
-    private static String dbUrl = System.getProperty("DB_HOST", "localhost").equals("host.docker.internal") ? DOCKER_URL : LOCAL_URL;
-    private static final String USER = "root";
-    private static final String PASSWORD = "llave";
+    private static final String URL = System.getenv("DB_URL") != null ? System.getenv("DB_URL") : "jdbc:mysql://localhost:3307/library_db";
+    private static final String USER = System.getenv("DB_USER") != null ? System.getenv("DB_USER") : "root";
+    private static final String PASSWORD = System.getenv("DB_PASSWORD") != null ? System.getenv("DB_PASSWORD") : "llave";
+
 
     public static Connection getConnection() throws SQLException {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            return DriverManager.getConnection(dbUrl, USER, PASSWORD);
+            return DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (ClassNotFoundException | SQLException e) {
             throw new SQLException("Database connection error", e);
         }
